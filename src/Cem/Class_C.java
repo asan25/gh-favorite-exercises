04b5b74dc4e234a4a63bfbf498cfb4ceacf3b0b6
Asan Ahmadli <asenahmedli@gmail.com>
2022-03-25 16:03:30 -0400

    first commit

---
+++ src/Cem/Class_C.java
@@ -0,0 +1,10 @@
+package Cem;
+
+public class Class_C {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Mehmet/Class_M.java
@@ -0,0 +1,10 @@
+package Mehmet;
+
+public class Class_M {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
