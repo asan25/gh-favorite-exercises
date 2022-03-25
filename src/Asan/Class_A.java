55b6a1397771846706323fae51987b432d549b2a
Asan Ahmadli <asenahmedli@gmail.com>
2022-03-25 16:29:04 -0400

    Second commit

--- src/Asan/Class_A.java
+++ src/Asan/Class_A.java
@@ -3,8 +3,10 @@ package Asan;
 public class Class_A {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 		System.out.println("Hello World");
-	}
+		System.out.println("Hello World 2");
+		
+	}//main
 
 }
